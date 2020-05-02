fa3d3350991793fdf9abbdc6cd895637c890214f
Yi Ou <ouyi@gmx.de>
2020-05-02 19:27:27 +0200

    Installed graphql-tools

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+import resolvers from "./resolvers";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}));
+
+app.listen(8080, () => console.log("Server running on localhost:8080/graphql"));
