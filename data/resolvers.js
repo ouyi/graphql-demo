082ecb1a855e0371fec43c9383a790952605f7ec
Yi Ou <ouyi@gmx.de>
2020-05-03 11:33:38 +0200

    Add more mutations and queries and fix Promise usage

--- data/resolvers.js
+++ data/resolvers.js
@@ -2,8 +2,16 @@ import { Friend, Alien } from './connectors';
 
 export const resolvers = {
     Query: {
-        getFriend: ({ id }) => {
-            return new Friend(id, friendDB[id]);
+        getFriend: (root, { id }) => {
+            return new Promise((resolve, reject) => {
+                Friend.findById(id, (err, res) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(res);
+                    }
+                });
+            });
         },
         getAliens: () => {
             return Alien.findAll();
@@ -23,12 +31,32 @@ export const resolvers = {
 
             friend.id = friend._id;
 
-            return new Promise((resolve, object) => {
+            return new Promise((resolve, reject) => {
                 friend.save((err) => {
                     if (err)
                         reject(err)
                     else
                         resolve(friend)
+                });
+            });
+        },
+        updateFriend: (root, { input }) => {
+            return new Promise((resolve, reject) => {
+                Friend.findOneAndUpdate({ _id: input.id }, input, { new: true }, (err, res) => {
+                    if (err)
+                        reject(err)
+                    else
+                        resolve(res);
+                });
+            });
+        },
+        deleteFriend: (root, { id }) => {
+            return new Promise((resolve, reject) => {
+                Friend.remove({ _id: id }, (err) => {
+                    if (err)
+                        reject(err)
+                    else
+                        resolve("Successfully deleted friend: " + id);
                 });
             });
         },
